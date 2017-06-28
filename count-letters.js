3d32e813d130947e42c2741564695c1d4d51c4bd
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-06-28 18:24:54 +0000

    sanity check

--- count-letters.js
+++ count-letters.js
@@ -1,3 +1,10 @@
-function countLetters(){
+var input = process.argv.splice(2);
 
-}
+function countLetters(input){
+  var noSpaces = input.toLowerCase().split(" ").join("");
+
+  var characterReturn = {
+    letter: letter,
+    times: times,
+  };
+} hi
