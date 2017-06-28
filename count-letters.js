fe3fa18f83e4ebf1f07b1ce1fad34550484c9f70
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-06-28 20:26:07 +0000

    corrected forEach loop and if statement and renamed input variable to differ from function

--- count-letters.js
+++ count-letters.js
@@ -1,11 +1,36 @@
+// function countLetters(input){
 
-function countLetters(input){
-  var letters = input.split(" ").join("");
+//   var result = {};
+
+//   for(var i = 0; i < input.length; i++){
+//     if(result[input[i]] != null){
+//       result[input[i]] += 1;
+//     } else {
+//       result[input[i]] = 1;
+//     }
+
+//   }
+// }
+
+// console.log(countLetters("lighthouse in the house"));
+
+
+
+//letters need to be keys and instances values of those letters(keys)
+//arrays can be values, so push instances of letters to array?
+//keys are always strings
+var givenInput = process.argv.slice(2).join("").toLowerCase().split("");
+
+function countLetters(input) {
   var result = {};
 
-  for(var i = 0; i < letters.length; i++){
-    if(
-  }
+  input.forEach(function(letter){
+    if(result[letter] === undefined) result[letter] = 0;
+    result[letter] += 1;
+   });
+
+ return result;
+
 }
 
-countLetters("lighthouse in the house");
+console.log(countLetters(givenInput));
